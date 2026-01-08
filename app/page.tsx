e53421469ba415fa164004e02c0d227fdd730edb
CodewithMukal <mukal.github@gmail.com>
2026-01-08 16:58:06 +0530

    feat(landing): Added landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,35 @@
+import Aurora from "@/components/ui/Aurora";
+import { Button } from "@/components/ui/button";
+import Particles from "@/components/ui/Particles";
+import LightRays from "@/components/ui/Rays";
+import { Pixelify_Sans } from "next/font/google";
 import Image from "next/image";
+import Link from "next/link";
+
+const pixelify = Pixelify_Sans({subsets: ["latin"]})
 
 export default function Home() {
+  const buttonTextList = [
+    "Take me through..",
+    "Start Focusing",
+    "Let's Begin",
+  ]
+  const bgList = [<Aurora speed={0.3}/>, <LightRays/>, <Particles particleColors={["#11111"]}/>]
+  const randomBgIndex = Math.floor(Math.random()* bgList.length)
+  const randomIndex = Math.floor(Math.random()*buttonTextList.length);
+  const randomText = buttonTextList[randomIndex];
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <div className="w-full min-h-screen justify-center items-center flex flex-col text-white">
+        <div className="fixed top-0 left-0 -z-10 opacity-50">
+          {bgList[randomBgIndex]}
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+        <h1 className={`${pixelify.className} text-4xl`}>FOCUS.</h1>
+        <p className="text-sm">Timers, ambient music, non-distractive backgrounds - so YOU can lock tf in...</p>
+        <Link href={"/focus"}>
+          <Button className={`my-5 duration-600 hover:ring-1 ring-white/20 ${pixelify.className} cursor-pointer`} >{randomText}</Button>
+        </Link>
+      </div>  
+    </>
   );
 }
